e8b2683088c136a914335518aecb268fec4ebb51
Anderson Frias <anderl.friaas@gmail.com>
2025-03-27 17:41:31 -0400

    [Add] Calculo del padding con save-area-context

--- src/app/_layout.js
+++ src/app/_layout.js
@@ -1,14 +1,14 @@
 import { Slot } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function Layout() {
+  const insets = useSafeAreaInsets();
   return (
-    <View>
+    <View style={{ flex: 1, backgroundColor: "#000", paddingTop: insets.top, paddingBottom: insets.bottom }}>
       <StatusBar style="light" backgroundColor="#000" barStyle="light-content" />
-        <View style={{ justifyContent: 'center', alignItems: 'center', backgroundColor: '#000', height: '100%'}}>
-          <Slot />
-        </View>
+      <Slot />
     </View>
   );
 }
